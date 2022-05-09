7c161e7b04f3805d71ed7ea2f8dac4b96c85c929
Rijo George <rijogeorge@rijos-mbp.lan>
2022-05-09 09:41:09 -0700

    bubble sort done

--- bubble.js
+++ bubble.js
@@ -1,3 +1,20 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+  for (let j = 0; j < arr.length - 1; j++) {
+    let swap = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        swap = true;
+        const temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+      }
+    }
+    if (!swap) {
+      break;
+    }
+    swap = false;
+  }
+  return arr;
+}
 
 module.exports = bubbleSort;
